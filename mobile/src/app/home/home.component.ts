0d8479b6d18e46db92102c0f3d2726a88a6277a0
jkierucz <01169183@pw.edu.pl>
2025-11-02 00:11:47 +0100

    project structure

--- mobile/src/app/home/home.component.ts
+++ mobile/src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import {
-  NativeScriptCommonModule,
-  NativeScriptRouterModule,
-} from '@nativescript/angular';
+import { registerElement } from '@nativescript/angular';
+import { RouterLink } from '@angular/router';
+import { Image } from '@nativescript/core';
+
+registerElement('Image', () => Image);
 
 @Component({
-  selector: 'Home',
+  standalone: true,
+  selector: 'ns-home',
   templateUrl: './home.component.html',
-  imports: [NativeScriptCommonModule, NativeScriptRouterModule],
+  styleUrls: ['./home.component.css'],
+  imports: [RouterLink],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
