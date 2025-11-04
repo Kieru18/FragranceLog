e39325fd838f67ebdd293a26a83b3f927dec8578
Jakub Kieruczenko <jakub.kieruczenko18@gmail.com>
2025-11-04 23:04:54 +0100

    changed app signature

--- mobile/nativescript.config.ts
+++ mobile/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.mobile',
+  id: 'org.kieru.fragrancelog',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
