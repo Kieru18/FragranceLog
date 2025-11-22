9a262c5d2a16a6691c9b21cc708e119f4d7ffe80
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-11-22 15:19:09 -0800

    ef scaffolding

---
+++ backend/Core/Entities/Gender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Entities;
+
+[Index("Name", Name = "UQ_Genders_Name", IsUnique = true)]
+public partial class Gender
+{
+    [Key]
+    public int GenderId { get; set; }
+
+    [StringLength(6)]
+    public string Name { get; set; } = null!;
+
+    [InverseProperty("Gender")]
+    public virtual ICollection<PerfumeGenderVote> PerfumeGenderVotes { get; set; } = new List<PerfumeGenderVote>();
+
+    [InverseProperty("Gender")]
+    public virtual ICollection<Perfume> Perfumes { get; set; } = new List<Perfume>();
+}
