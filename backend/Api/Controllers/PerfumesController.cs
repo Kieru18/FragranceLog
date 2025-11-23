c6a8ab8f835ba196e5182ff9d910b21f9ebf1844
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-11-23 14:01:50 -0800

    renamed controller

---
+++ backend/Api/Controllers/PerfumesController.cs
@@ -0,0 +1,30 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PerfumesController : ControllerBase
+    {
+        private readonly FragranceLogContext _context;
+
+        public PerfumesController(FragranceLogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPerfumes()
+        {
+            var perfumes = await _context.Perfumes
+                .Take(20)
+                .ToListAsync();
+
+            return Ok(perfumes);
+        }
+    }
+}
