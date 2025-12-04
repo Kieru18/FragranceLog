7d896cfec8b4dae09445d561771f23303c34dcb4
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-04 13:14:49 +0100

    configuration for new DataImporter

---
+++ backend/DataImporter/Configuration/ImportOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace DataImporter.Configuration;
+
+public class ImportOptions
+{
+    public string CsvPath { get; set; } = null!;
+    public ImporterUserOptions ImporterUser { get; set; } = new();
+}
+
+public class ImporterUserOptions
+{
+    public string Password { get; set; } = null!;
+}
