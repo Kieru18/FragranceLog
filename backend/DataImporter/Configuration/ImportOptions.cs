c95220f3d8b362506e450cee2ec7318ce71d82a6
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-04 20:24:15 +0100

    added user count param

--- backend/DataImporter/Configuration/ImportOptions.cs
+++ backend/DataImporter/Configuration/ImportOptions.cs
@@ -3,6 +3,7 @@
 public class ImportOptions
 {
     public string CsvPath { get; set; } = null!;
+    public int FakeUserCount { get; set; } = 50;
     public ImporterUserOptions ImporterUser { get; set; } = new();
 }
 
