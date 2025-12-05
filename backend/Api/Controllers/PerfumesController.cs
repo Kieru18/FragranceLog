209c235b1c43ac9afd5317b4776c2ddff38f9624
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-05 16:28:15 +0100

    base search

--- backend/Api/Controllers/PerfumesController.cs
+++ backend/Api/Controllers/PerfumesController.cs
@@ -1,7 +1,9 @@
+using Core.DTOs;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Controllers
 {
@@ -17,14 +19,51 @@ namespace Api.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetPerfumes()
+        [HttpGet("search")]
+        public async Task<ActionResult<List<PerfumeSearchResultDto>>> Search(
+        [FromQuery] PerfumeSearchRequestDto req)
         {
-            var perfumes = await _context.Perfumes
-                .Take(20)
+            var q = req.Query.Trim().ToLower();
+
+            var query = _context.Perfumes
+                .AsNoTracking()
+                .Where(p =>
+                    string.IsNullOrEmpty(q) ||
+                    p.Name.ToLower().Contains(q));
+
+            if (req.BrandId != null)
+                query = query.Where(p => p.BrandId == req.BrandId);
+
+            if (!string.IsNullOrEmpty(req.CountryCode))
+                query = query.Where(p => p.CountryCode == req.CountryCode);
+
+            var results = await query
+                .Select(p => new
+                {
+                    p,
+                    Rating = p.Reviews.Any()
+                        ? p.Reviews.Average(r => r.Rating)
+                        : 0,
+                    RatingCount = p.Reviews.Count
+                })
+                .OrderByDescending(x =>
+                    x.p.Name.ToLower().StartsWith(q) ? 3 :
+                    x.p.Name.ToLower().Contains(q) ? 2 : 1)
+                .ThenByDescending(x => x.Rating)
+                .Skip((req.Page - 1) * req.PageSize)
+                .Take(req.PageSize)
+                .Select(x => new PerfumeSearchResultDto
+                {
+                    PerfumeId = x.p.PerfumeId,
+                    Name = x.p.Name,
+                    Brand = x.p.Brand.Name,
+                    Country = x.p.CountryCode,
+                    Rating = Math.Round(x.Rating, 2),
+                    RatingCount = x.RatingCount
+                })
                 .ToListAsync();
 
-            return Ok(perfumes);
+            return results;
         }
     }
 }
