0b991391734bd30ff1614100f9f02726b917ddda
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-06 17:08:38 +0100

    moved logic to service

--- backend/Api/Controllers/PerfumesController.cs
+++ backend/Api/Controllers/PerfumesController.cs
@@ -1,9 +1,7 @@
 using Core.DTOs;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Controllers
 {
@@ -12,75 +10,20 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class PerfumesController : ControllerBase
     {
-        private readonly FragranceLogContext _context;
+        private readonly IPerfumeService _perfumeService;
 
-        public PerfumesController(FragranceLogContext context)
+        public PerfumesController(IPerfumeService perfumeService)
         {
-            _context = context;
+            _perfumeService = perfumeService;
         }
 
         [HttpPost("search")]
         public async Task<ActionResult<PerfumeSearchResponseDto>> Search(
             [FromBody] PerfumeSearchRequestDto req,
-        CancellationToken ct)
+            CancellationToken ct)
         {
-            var page = req.Page <= 0 ? 1 : req.Page;
-            var pageSize = req.PageSize is <= 0 or > 50 ? 25 : req.PageSize;
-
-            var q = req.Query?.Trim().ToLower();
-
-            var query =
-                from p in _context.Perfumes.AsNoTracking()
-                join r in _context.Reviews on p.PerfumeId equals r.PerfumeId into ratings
-                select new
-                {
-                    Perfume = p,
-                    AvgRating = ratings.Any() ? ratings.Average(x => x.Rating) : 0,
-                    RatingCount = ratings.Count()
-                };
-
-            if (!string.IsNullOrEmpty(q))
-            {
-                query = query.Where(x =>
-                    x.Perfume.Name.ToLower().Contains(q));
-            }
-
-            if (req.BrandId != null && req.BrandId != 0)
-                query = query.Where(x => x.Perfume.BrandId == req.BrandId);
-
-            if (!string.IsNullOrWhiteSpace(req.CountryCode))
-                query = query.Where(x => x.Perfume.CountryCode == req.CountryCode);
-
-            if (req.MinRating != null && req.BrandId != 0)
-                query = query.Where(x => x.AvgRating >= req.MinRating);
-
-            var totalCount = await query.CountAsync(ct);
-
-            var result = await query
-                .OrderByDescending(x =>
-                    q != null && x.Perfume.Name.ToLower().StartsWith(q) ? 3 :
-                    q != null && x.Perfume.Name.ToLower().Contains(q) ? 2 : 1)
-                .ThenByDescending(x => x.AvgRating)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .Select(x => new PerfumeSearchResultDto
-                {
-                    PerfumeId = x.Perfume.PerfumeId,
-                    Name = x.Perfume.Name,
-                    Brand = x.Perfume.Brand.Name,
-                    Rating = Math.Round(x.AvgRating, 2),
-                    RatingCount = x.RatingCount,
-                    CountryCode = x.Perfume.CountryCode
-                })
-                .ToListAsync(ct);
-
-            return Ok(new PerfumeSearchResponseDto
-            {
-                TotalCount = totalCount,
-                Page = page,
-                PageSize = pageSize,
-                Items = result
-            });
+            var result = await _perfumeService.SearchAsync(req, ct);
+            return Ok(result);
         }
     }
 }
