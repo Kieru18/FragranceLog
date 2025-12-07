ba6420484fa7f17969f4b90cac5bf4db51afc098
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-07 22:31:58 +0100

    moved footer into separate component

---
+++ mobile/src/app/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NativeScriptCommonModule, RouterExtensions } from '@nativescript/angular';
+
+@Component({
+  standalone: true,
+  selector: 'app-footer',
+  imports: [
+    NativeScriptCommonModule,
+    RouterModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent {
+    constructor(private routerExtensions: RouterExtensions) {}
+
+  goToSearch() {
+    this.routerExtensions.navigate(['/search']);
+  }
+
+  goToHome() {
+    this.routerExtensions.navigate(['/home']);
+  }
+}
