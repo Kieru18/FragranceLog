ba55a2ed58c6f23f9597112ddd736306ea1fe4e7
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2025-12-23 22:49:12 +0100

    removed unused import

--- mobile/src/app/services/sharedlist.service.ts
+++ mobile/src/app/services/sharedlist.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { SharedListDto } from '../models/sharedlist.dto';
 import { SharedListPreviewDto } from '../models/sharedlistpreview.dto';
 
 @Injectable({ 
