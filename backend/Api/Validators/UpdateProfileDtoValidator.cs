349a9fafdc8df016a60097ed1b43bd652e082d4c
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2026-01-02 13:16:45 +0100

    added dto validators

---
+++ backend/Api/Validators/UpdateProfileDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Core.DTOs;
+using FluentValidation;
+
+namespace Api.Validators;
+
+public sealed class UpdateProfileDtoValidator : AbstractValidator<UpdateProfileDto>
+{
+    public UpdateProfileDtoValidator()
+    {
+        RuleFor(x => x.DisplayName)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(50);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(100);
+    }
+}
